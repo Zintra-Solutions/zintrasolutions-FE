2847d85fe2ba0d148a0ffe9d8cfd0acad108bccb
Daniel Dada <simplytobs@gmail.com>
2026-07-25 18:24:16 +0100

    center the content

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 
 const Home = () => {
   return (
-    <main>
-      <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-3xl font-bold">Zintra Solutions</h1>
-        <p className="text-lg">Welcome to Zintra Solutions!</p>
-        <div>
+    <main className="min-h-screen">
+      <div className="flex flex-col items-center justify-between p-24">
+        <div className="w-full max-w-5xl font-mono text-sm text-center">
+          <h1 className="text-3xl font-bold">Zintra Solutions</h1>
+          <p className="text-lg">Welcome to Zintra Solutions!</p>
           <p className="text-md">🚧 Zintra Solutions under construction 🚧</p>
         </div>
       </div>
