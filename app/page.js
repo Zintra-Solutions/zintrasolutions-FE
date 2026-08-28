4a9ff71a94a00191ed3eb1abb2fe4fcc78235247
Daniel Dada <simplytobs@gmail.com>
2026-08-28 01:36:15 +0100

    feat: started implementing the hero section

--- app/page.js
+++ app/page.js
@@ -1,14 +1,9 @@
+import Hero from "@/components/Hero"
 
 const Home = () => {
   return (
     <main className="min-h-screen">
-      <div className="flex flex-col items-center justify-between p-24">
-        <div className="w-full max-w-5xl text-sm text-center">
-          <h1 className="text-3xl font-bold">Zintra Solutions</h1>
-          <p className="text-lg">Welcome to Zintra Solutions!</p>
-          <p className="text-md">🚧 Zintra Solutions under construction 🚧</p>
-        </div>
-      </div>
+      <Hero />
     </main>
   )
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,35 @@
+import Image from "next/image"
 
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <main className="min-h-screen mt-zintra-11">
+            <section className="">
+                <div className="max-w-4xl mx-auto flex flex-col items-center justify-center">
+                    <h1 className="text-display-1 font-bold text-center" >
+                        We Build Digital Experiences that <span className="text-zintra-primary">Inspire & perform</span>
+                    </h1>
+                    <p className="text-body-base max-w-3xl text-center mt-zintra-7" >
+                        We combine strategy, design, and technology to create websites, digital products, and brand experiences that help businesses stand out, connect with their audience, and grow with confidence.
+                    </p>
+                    <div className="flex gap-2 mt-zintra-9">
+                        <button className="bg-zintra-ink text-white px-8 py-5 rounded-full hover:bg-zintra-primary-dark" >
+                            Book a call
+                        </button>
+                        <button className="border border-zintra-ink  px-8 py-5 rounded-full" >
+                            See our work
+                        </button>
+                    </div>
+                </div>
+            </section>
+            <section className="relative mt-zintra-13 overflow-hidden">
+                <div className="bg-gradient absolute"></div>
+                <div className="relative z-10 flex items-center justify-center ">
+                    <Image src="/images/hero-phone.png" alt="Hero background" width={301} height={600} />
+                </div>
+
+            </section>
+        </main>
     )
 }
 export default Hero
